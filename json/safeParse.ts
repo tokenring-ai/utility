694787b4e1b57c715a1929465ff23923b891582f
Mark Dierolf <mdierolf@cloudquote.io>
2026-03-18 21:55:07 -0700

    Add `safeParse` utility for JSON parsing with default fallback

---
+++ json/safeParse.ts
@@ -0,0 +1,7 @@
+export default function safeParse<T>(jsonString: string, defaultValue: T): T {
+  try {
+    return JSON.parse(jsonString) as T;
+  } catch (error) {
+    return defaultValue;
+  }
+}
